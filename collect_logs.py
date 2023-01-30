f07bd4b09f85b054edf3712ae0f56a9315324751
Rafael Chaves <rafaelvchaves@gmail.com>
2023-01-30 13:55:30 -0500

    Record failures in match log script

--- collect_logs.py
+++ collect_logs.py
@@ -9,11 +9,15 @@ import urllib
 import time
 import random
 import requests
+import os
 
+failures = []
 
 def fetch_player_stats(player, stats_type=['summary']):
     name = player['web_name']
     fbref_id = player['fbref_id']
+    fpl_id = player['id']
+    player_file = f'data/match_logs/{fpl_id}.csv'
     dfs = []
     for st in stats_type:
         time.sleep(1)
@@ -22,11 +26,16 @@ def fetch_player_stats(player, stats_type=['summary']):
             df = pd.read_html(url)[0]
             # remove multi-indexed columns
             df.columns = [c[1] for c in df.columns]
-            df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
             df = df[~df['Date'].isna()]
             df.drop(['Day', 'Pos', 'Match Report'], axis=1, inplace=True)
+            # if os.path.exists(player_file):
+            #     prev_df = pd.read_csv(player_file)
+            #     if len(prev_df) == len(df):
+            #         # already up-to-date, no need to make more calls
+            #         return
             dfs.append(df)
         except Exception as e:
+            failures.append(fpl_id)
             print(f'{st} stats for player {name} not found: {e}')
             return
     if len(dfs) == 0:
@@ -34,8 +43,7 @@ def fetch_player_stats(player, stats_type=['summary']):
     df = pd.concat(dfs, axis=1)
     df = df.loc[:, ~df.columns.duplicated()].copy()
     df = df.replace('On matchday squad, but did not play', float('nan'))
-    fpl_id = player['id']
-    df.to_csv(f'data/match_logs/{fpl_id}.csv', index=False)
+    df.to_csv(player_file, index=False)
 
 
 players = pd.read_csv('players_2022-23.csv')
@@ -55,3 +63,5 @@ for player in players.to_dict(orient='records'):
         time.sleep(max(0, 60 - elapsed))
         calls = 0
         start = time.perf_counter()
+
+print(failures)
